136417524b0ba72110d343c59967de5c5dfd1a5c
Victor Guleshov <victorguleshov@gmail.com>
2024-01-17 21:22:09 +0800

    move some editor features for editor folder

--- Editor/Features/ClearAllFeature.cs
+++ Editor/Features/ClearAllFeature.cs
@@ -2,30 +2,24 @@ using System.IO;
 using UnityEditor;
 using UnityEngine;
 
-namespace VG.Features
+namespace VG.Editor.Features
 {
-    public static class PathFeature
+    public static class ClearAllFeature
     {
-#if UNITY_EDITOR
         [MenuItem("Tools/VG/Clear All")]
-#endif
         public static void ClearAll()
         {
             ClearPlayerPrefs();
             ClearPersistentData();
         }
 
-#if UNITY_EDITOR
         [MenuItem("Tools/VG/Clear Player Prefs")]
-#endif
         public static void ClearPlayerPrefs()
         {
             PlayerPrefs.DeleteAll();
         }
 
-#if UNITY_EDITOR
         [MenuItem("Tools/VG/Clear Persistent Data")]
-#endif
         public static void ClearPersistentData()
         {
             foreach (var directory in Directory.GetDirectories(Application.persistentDataPath))
@@ -40,14 +34,5 @@ namespace VG.Features
                 fileInfo.Delete();
             }
         }
-
-#if UNITY_EDITOR
-        [MenuItem("Tools/VG/Open Persistent Data Path")]
-        public static void OpenPersistentDataPath()
-        {
-            EditorUtility.RevealInFinder(Application.persistentDataPath);
-        }
-
-#endif
     }
 }
